1c7a3a8f336b4286d8d76d0bad4eb9ce8aca36cb
Skyler Dakota Lawton <skylerdakota@gmail.com>
2020-10-28 11:09:53 -0400

    generated prompts for user

---
+++ Develop/README.md
@@ -0,0 +1,8 @@
+# Cody is the best dog
+    # this is a README file generated through node.js
+    # install the npm package and then run 'node index.js' in the terminal when in the correct project directory
+    # to demonstrate the utility of the terminal for file creation outside of the browser environment
+    # undefined
+    # run index.js in the terminal and if you are prompted it is working
+    # cody is a cool dog 
+    # skylerdakota
--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,114 @@
-// array of questions for user
-const questions = [
+// NPM packages and standard libraries to require in
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// Promises for handling asynchronous behavior
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // function to initialize program
-function init() {
+function promptUser() {
+    // array of questions for user
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projecttitle",
+            message: "What is the title of your project?"
+          },
+          {
+            type: "input",
+            name: "description",
+            message: "What is the description of your project?"
+          },
+          {
+            type: "input",
+            name: "install",
+            message: "What are the installation instructions?"
+          },
+          {
+            type: "input",
+            name: "use",
+            message: "What is the usage information?"
+          },
+          {
+            type: "input",
+            name: "contributions",
+            message: "What are the contribibutions guidelines?"
+          },
+          {
+            type: "input",
+            name: "test",
+            message: "What are the test instructions?"
+          },
+          {
+            type: "checkbox",
+            message: "Which license is your application covered under?",
+            name: "license",
+            choices: [
+              "License 1", 
+              "License 2", 
+              "License 3", 
+              "License 4"
+            ]
+          },
+          {
+            type: "input",
+            name: "github",
+            message: "What is your github username?"
+          }
+        ]);
+      }
 
-}
+// function to generate markdown for README
+function generateREADME(answers) {
+    return `# ${answers.projecttitle}
+    ## Project Description
+    * ${answers.description}
+    ## Installation Instructions
+    * ${answers.install}
+    ## Usage Information
+    * ${answers.use}
+    ## Contributor Guidelines
+    * ${answers.contribution}
+    ## Test Instructions
+    * ${answers.test}
+    ## Project Licesnse
+    * ${answers.license}
+    ## Project Creator
+    * ${answers.github}`;
+    }
+    
+    //module.exports = generateMarkdown;
 
 // function call to initialize program
-init();
+promptUser()
+  .then(function(answers) {
+    const readme = generateREADME(answers);
+
+    // function to write README file
+    return writeFileAsync("README.md", readme);
+  })
+  .then(function() {
+    console.log("Successfully wrote to README.md");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+
+//   // array of questions for user
+// const questions = [
+
+// ];
+
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
